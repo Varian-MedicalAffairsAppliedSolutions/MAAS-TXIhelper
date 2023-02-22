c2cd0ba2f00a722fbff92cf1cb1b2b6364851e92
craman96 <craman96@gmail.com>
2023-02-22 04:48:49 -1200

    Updated pipeline

--- ModulationComplexity/Properties/AssemblyInfo.cs
+++ ModulationComplexity/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Template2")]
+[assembly: AssemblyTitle("ModulationComplexity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Template2")]
+[assembly: AssemblyProduct("ModulationComplexity")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
