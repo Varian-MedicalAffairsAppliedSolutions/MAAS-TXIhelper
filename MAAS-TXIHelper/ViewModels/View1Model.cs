f9ca669f1070b0d1894723e2e0b73ecaac72551c
craman96 <craman96@VIC.COM>
2023-06-21 20:24:06 +0000

    Added logic for determining beam type

--- MAAS-TXIHelper/ViewModels/View1Model.cs
+++ MAAS-TXIHelper/ViewModels/View1Model.cs
@@ -42,6 +42,48 @@ namespace ViewModels
         public DelegateCommand FlipCmd { get; set;} 
         public DelegateCommand SelectLogPathCmd { get; set;}
 
+        private bool _IsStaticBeamPlan;
+
+        public bool IsStaticBeamPlan
+        {
+            get { return _IsStaticBeamPlan; }
+            set { SetProperty(ref _IsStaticBeamPlan, value); }
+        }
+
+        private bool _IsDynamicBeamPlan;
+
+        public bool IsDynamicBeamPlan
+        {
+            get { return _IsDynamicBeamPlan; }
+            set { SetProperty(ref _IsDynamicBeamPlan, value); }
+        }
+
+        private bool _IsArcBeamPlan;
+
+        public bool IsArcBeamPlan
+        {
+            get { return _IsArcBeamPlan; }
+            set { SetProperty(ref _IsArcBeamPlan, value); }
+        }
+
+        private bool _IsSX2MLC;
+
+        public bool IsSX2MLC
+        {
+            get { return _IsSX2MLC; }
+            set { SetProperty(ref _IsSX2MLC, value); }
+        }
+
+        private bool _IsHalcyon;
+
+        public bool IsHalcyon
+        {
+            get { return _IsHalcyon; }
+            set { SetProperty(ref _IsHalcyon, value); }
+        }
+
+
+
         public View1Model(ScriptContext currentContext)
         {
 
@@ -52,6 +94,42 @@ namespace ViewModels
 
             FlipCmd = new DelegateCommand(OnFlip);
             SelectLogPathCmd = new DelegateCommand(OnSelectLogPath);
+
+            // Determine what type of mlc and technique we are using
+            _IsStaticBeamPlan = false;
+            _IsDynamicBeamPlan = false;
+            _IsArcBeamPlan = false;
+            _IsSX2MLC = false;
+            _IsHalcyon = false;
+            foreach (var beam in _Plan.Beams)
+            {
+                if (beam.IsSetupField == false && beam.IsImagingTreatmentField == false)
+                {
+                    if (beam.Technique.ToString() == "STATIC")
+                    {
+                        // Here we assume that if one treatment beam is a static beam, then all the beams
+                        // in this treatment plan must be static beams.
+                        _IsStaticBeamPlan = true;
+                    }
+                    if (beam.Technique.ToString().ToLower().Contains("arc"))
+                    {
+                        // Here we assume that if one treatment beam is a VMAT or arc beam, then all the beams
+                        // in this treatment plan must be VMAT or arc beams.
+                        _IsArcBeamPlan = true;
+                    }
+                    if (beam.MLC.Model == "SX2")
+                    {
+                        // Here we assume that it is a Halcyon or Ethos linac, because the "SX2" MLC only
+                        // appears on such linacs.
+                        _IsSX2MLC = true;
+                    }
+                    if (beam.TreatmentUnit.MachineModel == "RDS")
+                    {
+                        // Here we assume that it is a Halcyon or Ethos linac
+                        _IsHalcyon = true;
+                    }
+                }
+            }
 
         }
 
